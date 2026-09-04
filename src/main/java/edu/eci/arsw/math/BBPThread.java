15621d012cad75fb4862f0a98099b1e2dab8e2fd
unknown <mariana.malagon-t@labinfo.is.escuelaing.edu.co>
2026-09-04 11:07:39 -0500

    feat: point 1 finished

--- src/main/java/edu/eci/arsw/math/BBPThread.java
+++ src/main/java/edu/eci/arsw/math/BBPThread.java
@@ -6,7 +6,7 @@ public class BBPThread extends Thread{
     public byte [] digits;
 
 
-    public BBPThread{
+    public BBPThread (){
         this.start = start;
         this.count = count;
     }
